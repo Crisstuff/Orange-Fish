c498964f8c13f7389323a0c33ebecbf662520c9c
Crisstuff <148343622+Crisstuff@users.noreply.github.com>
2024-05-06 11:36:41 +0200

    i made the basic site and started with scroll til sction function on content

---
+++ story.js
@@ -0,0 +1,19 @@
+let previousSection = null;
+
+function scrollToSection(sectionId) {
+    window.onload = addStorySection("content", "my-favoritt");
+    let section = document.getElementById(sectionId);
+    if (section) {
+        // Skjul den forrige seksjonen hvis den eksisterer
+        if (previousSection) {
+            previousSection.style.display = 'none';
+        }
+        // Vis mål-avsnittet
+        section.style.display = 'flex';
+        // Rull til mål-avsnittet
+        section.scrollIntoView({ behavior: 'smooth', block: 'start' });
+        // Oppdater den forrige seksjonen
+        previousSection = section;
+    }
+}
+
