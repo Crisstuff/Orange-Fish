38eecc1cf9ede87a77369d09f2db914bffd6134b
Crisstuff <148343622+Crisstuff@users.noreply.github.com>
2024-05-28 18:45:54 +0200

    stuff

--- story.js
+++ story.js
@@ -1,22 +1,12 @@
-let previousSection = null;
 
-function scrollToSection(sectionId) {
-    window.onload = addStorySection("content", "my-favoritt");
-    let section = document.getElementById(sectionId);
-    if (section) {
-        // Skjul den forrige seksjonen hvis den eksisterer
-        if (previousSection) {
-            previousSection.style.display = 'none';
-        }
-        // Vis mål-avsnittet
-        section.style.display = 'flex';
-        // Rull til mål-avsnittet
-        section.scrollIntoView({ behavior: 'smooth', block: 'start' });
-        // Oppdater den forrige seksjonen
-        previousSection = section;
-    }else{
-        window.onload = addStorySection("content", "my-favoritt");
-    }
+/* Open */
+function openNav() {
+  document.getElementById("myNav").style.height = "100%";
+}
+
+/* Close */
+function closeNav() {
+  document.getElementById("myNav").style.height = "0%";
 }
 
 
@@ -48,3 +38,5 @@ async function initMap() {
 }
 
 initMap();
+
+
